708ff4b6983dca0b94789d089e34b892fe282992
Iiro Partanen <iispar@utu.fi>
2023-12-16 14:56:12 +0200

    test databse inject template

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
-INSERT INTO accounts (account_name, account_username, account_email, account_password) VALUES ('test name', 'test username', 'test email', '$2a$10$0ooeofhWH.dzRKtwBUI6l.mcPaj7TZAoBcNpHUEFmeQhY0SPYecd6');
+INSERT INTO account (account_name, account_username, account_email, account_password) VALUES ('test name', 'test username', 'test email', '$2a$10$0ooeofhWH.dzRKtwBUI6l.mcPaj7TZAoBcNpHUEFmeQhY0SPYecd6');
+
+INSERT INTO type (type_name) VALUE ('test type');
+INSERT INTO country (country_name) VALUE ('test country');
+INSERT INTO category (category_name) VALUE ('test category');
+INSERT INTO ingredient (ingredient_name) VALUE ('test ingredient');
+INSERT INTO unit (unit_name) VALUE ('test unit');
