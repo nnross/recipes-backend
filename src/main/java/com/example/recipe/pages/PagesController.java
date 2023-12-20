aa8c762205cb7eea16fc92c2b980b267166addaf
Iiro Partanen <iispar@utu.fi>
2023-12-20 14:44:27 +0200

    personal page call

--- src/main/java/com/example/recipe/pages/PagesController.java
+++ src/main/java/com/example/recipe/pages/PagesController.java
@@ -1,9 +1,28 @@
 package com.example.recipe.pages;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.recipe.recipe.Recipe;
+import com.example.recipe.recipe.RecipeService;
+import com.example.recipe.response.PersonalPageRes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pages")
 public class PagesController {
+    @Autowired
+    private PagesService pagesService;
+
+    /**
+     * API GET call to /api/pages/get/personal?accountId=(id)
+     * will return the personal page for the selected account.
+     * @param accountId
+     *        id of account we want.
+     * @return personal page data.
+     */
+    @PreAuthorize("#accountId == authentication.principal.id")
+    @GetMapping("/get/personal")
+    public PersonalPageRes add(@RequestParam int accountId) {
+        return pagesService.getPersonal(accountId);
+    }
 }
--- src/main/java/com/example/recipe/pages/PagesService.java
+++ src/main/java/com/example/recipe/pages/PagesService.java
@@ -1,7 +1,42 @@
 package com.example.recipe.pages;
 
+import com.example.recipe.account.AccountRepository;
+import com.example.recipe.recipe.Day;
+import com.example.recipe.recipe.Recipe;
+import com.example.recipe.recipe.RecipeService;
+import com.example.recipe.recipe.RecipeStats;
+import com.example.recipe.response.ListRes;
+import com.example.recipe.response.PersonalPageRes;
+import exceptions.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PagesService {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+
+    /**
+     * Gets the personal page data and constucts a PersonalPageRes.
+     * @param accountId
+     *        id of account we want data for.
+     * @return personal page data as PersonalPageRes.
+     */
+    public PersonalPageRes getPersonal(int accountId) {
+        accountRepository.findById(accountId).orElseThrow(() ->
+                new BadRequestException("no account with id"));
+
+        ListRes recipes = recipeService.getFavourite(accountId, 0);
+        RecipeStats stats = recipeService.getStats(accountId);
+        Map<String, Day> calendar = recipeService.getCalendar(accountId);
+
+        return new PersonalPageRes(recipes, stats, calendar);
+    };
 }
